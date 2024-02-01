5c44e5e798c09e5c0903c07814945ff1a8ff9e13
shmoon <ssy6308msh2@naver.com>
2024-02-01 11:07:24 +0900

    첫번째 유니티 프로젝트

---
+++ Assets/Scrips/ObjectHit.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectHit : MonoBehaviour
+{
+    // 벽에 부딪힐때 정보를 출력하는 메소드(oncollisionenter)
+   private void OnCollisionEnter(Collision other) 
+   {
+          if(other.gameObject.tag == "Player")
+          {
+                // 컴포넌트<원하는 컴포넌트>().물질.색 = 색을 빨강색으로 바꾸는 기능
+                GetComponent<MeshRenderer>().material.color = Color.red;
+                gameObject.tag = "Hit";
+          }
+       
+   }
+}
